0d87aad6d950f173127381fa7562413f3e650158
atqiyacode <atqiya@atqiyacode.com>
2022-06-27 13:31:29 +0700

    modify and hide some value of user model

--- backend-laravel/app/Models/User.php
+++ backend-laravel/app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -32,6 +33,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'created_at',
+        'updated_at',
+        "email_verified_at",
     ];
 
     /**
@@ -42,4 +46,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected $appends = [
+        'has_verify'
+    ];
+
+    public function getHasVerifyAttribute()
+    {
+        return $this->email_verified_at ? true : false;
+    }
 }
