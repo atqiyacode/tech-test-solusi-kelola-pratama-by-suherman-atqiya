effdc015d57035006639e7e64255db4e767223f8
atqiyacode <atqiya@atqiyacode.com>
2022-06-27 13:10:59 +0700

    fix issues has role on user model

--- backend-laravel/database/seeders/DatabaseSeeder.php
+++ backend-laravel/database/seeders/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call(RolePermissionSeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
